0b9440c5689cd70bcec8e9537ca70baebf660413
Andireski110103 <125863798+Andireski110103@users.noreply.github.com>
2023-06-21 09:39:08 +0800

    Update firebase.js

--- Backend - API/config/firebase.js
+++ Backend - API/config/firebase.js
@@ -5,13 +5,7 @@ require('firebase/database');
 
 // Add Firebase SDK Snippet
 const firebaseConfig = {
-  apiKey: "AIzaSyAmTvMWmj_NtbzeU5JGVke08VcPgoMoxAM",
-  authDomain: "c-project-387808.firebaseapp.com",
-  projectId: "c-project-387808",
-  databaseURL : "https://c-project-387808-default-rtdb.asia-southeast1.firebasedatabase.app",
-  storageBucket: "c-project-387808.appspot.com",
-  messagingSenderId: "66950882895",
-  appId: "1:66950882895:web:cb20b79f2ec4e25606ac3a"
+------------------- Secret-------------------
 };
 
 
